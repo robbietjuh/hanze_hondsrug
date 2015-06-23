4924a020e8abf1b858568635c3bdd5c274f8fb6b
Robert de Vries <r.devries@robbytu.net>
2015-06-23 21:42:49 +0200

    + Implemented the dashboard controller and a way to handle new incident creation

--- src/main/app/controllers/FrontendController.php
+++ src/main/app/controllers/FrontendController.php
@@ -24,6 +24,11 @@ class FrontendController extends MvcBaseController {
     protected $account;
 
     /**
+     * @var HardwareComponentModel Reference to a shared HardwareComponentModel instance
+     */
+    protected $hc_model;
+
+    /**
      * Populates the controller's base variables
      * @param $sender MvcApplication The MvcApplication that dispatched to the controller
      */
@@ -36,7 +41,14 @@ class FrontendController extends MvcBaseController {
         // Check whether the user account has access
         $this->im->checkRole(1);
         $this->account = $this->im->getAccount();
+
+        // Set up user data
         $this->data['username'] = $this->account['gebruikersnaam'];
+        $this->data['displayname'] = $this->account['weergavenaam'];
+
+        // Load all components
+        $this->hc_model = $this->loadModel("HardwareComponentModel");
+        $this->data['componentlist'] = $this->hc_model->allObjects();
     }
 
     /**
@@ -44,6 +56,48 @@ class FrontendController extends MvcBaseController {
      * @param $args array URL params
      */
     public function dashboard($args) {
-        $this->renderView("frontend/header");
+        // Hand off the incident creation
+        $this->handleIncidentCreation();
+
+        // Render the dashboard view
+        $this->renderView("frontend/dashboard");
+    }
+
+    /**
+     * Checks for post data for incident creation and, when available, creates a
+     * new incident with the given information. If the incident data was invalid,
+     * it will set the viewdata's incident_error entry to a more detailed error message.
+     */
+    private function handleIncidentCreation() {
+        if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["create_incident"])) {
+            // Set up component ID list
+            $component_ids = array();
+            foreach($this->data['componentlist'] as $component)
+                $component_ids[] = $component['identificatiecode'];
+
+            // Check data input
+            if(!isset($_POST["component"]) || !isset($_POST["short_description"]) || !isset($_POST["description"]))
+                $this->data['incident_error'] = "U heeft niet alle velden ingevuld.";
+
+            else if(empty($_POST["component"]) || empty($_POST["short_description"]) || empty($_POST["description"]))
+                $this->data['incident_error'] = "U heeft niet alle velden ingevuld.";
+
+            else if(!in_array($_POST["component"], $component_ids))
+                $this->data['incident_error'] = "Het gekozen component bestaat niet.";
+
+            else {
+                // Load the incident model and create a new incident
+                $incident_model = $this->loadModel("IncidentModel");
+
+                $incident_model->createIncident(
+                    $_POST["component"],
+                    $_POST["short_description"],
+                    $_POST["description"],
+                    $this->account['id']
+                );
+
+                $this->data['incident_success'] = true;
+            }
+        }
     }
 }
