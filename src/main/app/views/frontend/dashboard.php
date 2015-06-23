6d5aeea210318728adf74f3ef6f8fd5eec6a435e
Robert de Vries <r.devries@robbytu.net>
2015-06-23 21:44:24 +0200

    + Added the base dashboard template

---
+++ src/main/app/views/frontend/dashboard.php
@@ -0,0 +1,14 @@
+<?=$this->renderView("frontend/header");?>
+
+        <div class="row">
+            <div class="col-md-8">
+                <h4>Waar heeft u hulp bij nodig?</h4>
+                <?=$this->renderView("frontend/list_qas");?>
+            </div>
+            <div class="col-md-4">
+                <h4>Meldt een nieuw incident</h4>
+                <?=$this->renderView("frontend/frm_incident");?>
+            </div>
+        </div>
+
+<?=$this->renderView("frontend/footer");?>
