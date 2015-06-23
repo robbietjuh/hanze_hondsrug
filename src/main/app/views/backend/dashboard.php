fd586a1de0763553c1d95bb690c5e65fc89a1c9a
Robert de Vries <r.devries@robbytu.net>
2015-06-24 00:05:24 +0200

    - Cleanup

--- src/main/app/views/backend/dashboard.php
+++ src/main/app/views/backend/dashboard.php
@@ -10,7 +10,6 @@
                 <?=$this->renderView("backend/menu");?>
             </div>
             <div class="col-md-8">
-                <h4>Overzicht van aangemelde incidenten</h4>
                 <?=$this->renderView("backend/list_incidents");?>
             </div>
         </div>
