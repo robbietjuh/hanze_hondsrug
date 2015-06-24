21b20c722a46837cd261e1e6956b581df9510d90
Robert de Vries <r.devries@robbytu.net>
2015-06-24 17:56:20 +0200

    + Added CI to the backend menu

--- src/main/app/views/backend/menu.php
+++ src/main/app/views/backend/menu.php
@@ -9,4 +9,9 @@
                         <h3 class="list-group-item-heading">Vragenlijsten</h3>
                         <p class="list-group-item-text">Beheer vragenlijsten en workarounds</p>
                     </a>
+                    <a href="/backend/hardware"
+                       class="list-group-item <?=($this->data['page'] == 'hardware') ? 'active' : '';?>">
+                        <h3 class="list-group-item-heading">Configuratiemanagement</h3>
+                        <p class="list-group-item-text">Beheer hardware en software</p>
+                    </a>
                 </ul>
