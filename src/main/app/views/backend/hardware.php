7c31617db7863036e7f85df79b1d269e7abbbad4
Robert de Vries <r.devries@robbytu.net>
2015-06-24 18:07:38 +0200

    + Added hardware list overview

---
+++ src/main/app/views/backend/hardware.php
@@ -0,0 +1,40 @@
+<?=$this->renderView("header");?>
+
+        <ol class="breadcrumb">
+            <li><a href="/backend">Backend</a></li>
+            <li class="active">Configuratiemanagement</li>
+        </ol>
+
+        <div class="row">
+            <div class="col-md-4">
+                <?=$this->renderView("backend/menu");?>
+            </div>
+            <div class="col-md-8">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th class="col-md-2">Soort</th>
+                            <th class="col-md-5">Identificatiecode</th>
+                            <th class="col-md-3">Merk</th>
+                            <th class="col-md-2">Locatie</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($this->data['hardwarelist'] as $ci) { ?>
+                        <tr>
+                            <td><?=ucfirst($ci['soort']);?></td>
+                            <td>
+                                <a href="/backend/hardware/<?=$ci['identificatiecode'];?>">
+                                    <?=strtoupper($ci['identificatiecode']);?>
+                                </a>
+                            </td>
+                            <td><?=ucfirst($ci['merk']);?></td>
+                            <td><?=ucfirst($ci['locatie']);?></td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+<?=$this->renderView("footer");?>
