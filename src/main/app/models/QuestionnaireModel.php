4a92657cf588ce1388da6c9bd454e4e1415faa8e
Robert de Vries <r.devries@robbytu.net>
2015-06-24 15:50:37 +0200

    + Implemented a createQuestionnaire method in the QuestionnaireModel model

--- src/main/app/models/QuestionnaireModel.php
+++ src/main/app/models/QuestionnaireModel.php
@@ -21,4 +21,30 @@ class QuestionnaireModel extends MvcBaseModel {
      * @var string Database table primary key field name for this model
      */
     protected $tablePrimaryKeyField = "id";
+
+    /**
+     * Creates a new questionnaire and returns the resulting entry
+     * @param string $title Title for the questionnaire
+     * @param string $icon Icon for the questionnaire
+     * @return array The resulting entry
+     * @throws Exception
+     */
+    public function createQuestionnaire($title, $icon) {
+        // Set up query
+        $query = $this->MvcInstance->db_conn->prepare(
+            "INSERT INTO {$this->tableName} SET" .
+            " titel = :title," .
+            " icoon = :icon," .
+            " datum = NOW()"
+        );
+
+        // .. and execute it
+        if($query->execute(array(
+            ':title' => $title,
+            ':icon' => $icon
+        )))
+            return $this->getObjectByPk($this->MvcInstance->db_conn->lastInsertId());
+        else
+            throw new Exception($query->errorInfo()[2]);
+    }
 }
