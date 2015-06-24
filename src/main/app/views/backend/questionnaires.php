8acadf0557d6fe5f93ab17ee461d4c6d6220593d
Robert de Vries <r.devries@robbytu.net>
2015-06-24 18:08:56 +0200

    * Refactored questionnaires backend view

--- src/main/app/views/backend/questionnaires.php
+++ src/main/app/views/backend/questionnaires.php
@@ -1,8 +1,8 @@
 <?=$this->renderView("header");?>
 
         <ol class="breadcrumb">
-            <li>Backend</li>
-            <li class="active">Dashboard</li>
+            <li><a href="/backend">Backend</a></li>
+            <li class="active">Vragenlijsten</li>
         </ol>
 
         <div class="row">
@@ -10,7 +10,40 @@
                 <?=$this->renderView("backend/menu");?>
             </div>
             <div class="col-md-8">
-                <?=$this->renderView("backend/list_questionnaires");?>
+                <?php if(count($this->data['questionnaires']) > 0) { ?>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th class="col-md-2">Datum</th>
+                            <th class="col-md-6">Titel</th>
+                            <th class="col-md-2">Icoon</th>
+                            <th class="col-md-2">Acties</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($this->data['questionnaires'] as $qs) { ?>
+                        <tr>
+                            <td><?=date_format(date_create($qs['datum']), "d-m-Y");?></td>
+                            <td>
+                                <a href="/questionnaire/<?=$qs['id'];?>">
+                                    <?=$qs['titel'];?>
+                                </a>
+                            </td>
+                            <td><i class="glyphicon glyphicon-<?=$qs['icoon'];?>"></i> <?=$qs['icoon'];?></td>
+                            <td>
+                                <a href="/backend/questionnaires/<?=$qs['id'];?>/delete" class="text-danger">
+                                    <i class="glyphicon glyphicon-trash"></i> Verwijderen
+                                </a>
+                            </td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+
+                <hr />
+                <?php } ?>
+
+                <a href="/backend/questionnaires/create" class="btn btn-default pull-right">Nieuwe vragenlijst</a>
             </div>
         </div>
 
